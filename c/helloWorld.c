fe5509dddcce33c3c1fb2cfc16fbfddba5c9776e
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-24 23:13:25 -0300

    C01 first C exercise Hello World

---
+++ c/helloWorld.c
@@ -0,0 +1,18 @@
+/*My first C program to print Hello, World!*/
+
+// pre-processor directive -include process before compilation process
+// <stdio.h> contains various input-output functions
+
+#include <stdio.h> 
+
+// int - integer primary type (tipo primitivo para inteiro) 
+// main () - function main that defines starting point of the program
+// printf () - function that is used to write text on screen
+// return 0 - <var> returns the code; return 0 means the code is end.
+
+int main()
+{
+    printf("Hello, World!");
+
+    return 0;
+}
