64bd451d44983390b129317392c5a709887867c3
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-25 01:21:58 -0300

    C01 creates exercise_1 with instructions and comment about pre processor

---
+++ c/basic-c-exercises/exercise_1.c
@@ -0,0 +1,13 @@
+// Write a C program to perform input/output of all basic data types
+// tipos primitivos >> var, const, float, double, char, etc...
+
+// Every C program is divided in 4 parts: 1) Pre processor directive,
+// 2) Main function 3) Assembly of compiled code and 4) Linking
+
+// include: pre processor and delivers the file i insert to our source code
+//          a small software thar accepts C source file and performs tasks
+//          such remove comments from the source code, macro expansion,
+//          expansion of included header files.
+
+#include <stdio.h>
+
