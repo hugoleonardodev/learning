b913c53c7c4f538f2b4988f904aa65c0823690b3
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-25 20:25:13 -0300

    Create js folder with js files from pre Trybe

---
+++ js/lastLetters.js
@@ -0,0 +1,7 @@
+function lastLetters(word) {
+    var lastLetter = word[(length)];
+    var secondLastLetter = word[(length-1)];
+    var lastLetters = lastLetter + " " + secondLastLetter  
+    return lastLetters
+    }
+    // função que retorna as duas ultimas letras de uma string em ordem invertida com espaço entre elas
