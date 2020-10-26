d149f9f65b91a030837aecc0ad8f1707cf08939d
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-26 16:57:21 -0300

    Creates megasena.ls with first part of the code

---
+++ js/megasena.js
@@ -0,0 +1,18 @@
+// Empty Array with random numbers
+let megaSenaNumbers = [];
+// Function Math.random() generates random numbers
+// from 1 to N floating
+let randomNumber = Math.random() * 60;
+// for a higher integer random number Math.ceil(randomNumber)
+console.log(randomNumber);
+console.log(Math.ceil(randomNumber));
+// if 0 was included, Math.ceil for lower integer
+// Variables receives numbers from 1 to 60
+let firstNumber;
+let secondNumber;
+let thirdNumber;
+let fourthNumber;
+let fifthNumber;
+let sixthNumber;
+
+
