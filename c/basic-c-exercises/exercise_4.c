2849426eb681e875449c488df2ce4bdf276e3bfa
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-25 22:48:20 -0300

    C program to receive length and height rectangle input and calculate its perimeter

---
+++ c/basic-c-exercises/exercise_4.c
@@ -0,0 +1,9 @@
+// lenght and height of a rectangle and perimeter
+
+#include <stdio.h>
+int main () {
+    int length;
+    printf("Enter length of the rectangle: ");
+    scanf("%d", &length);
+    printf("The length of the rectangle is %d", length);
+}
