405149eca2de321e1b1fed1d438b53fcb1248f9b
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-25 22:53:46 -0300

    Add a input for rectangle heigth

--- c/basic-c-exercises/exercise_4.c
+++ c/basic-c-exercises/exercise_4.c
@@ -5,5 +5,9 @@ int main () {
     int length;
     printf("Enter length of the rectangle: ");
     scanf("%d", &length);
-    printf("The length of the rectangle is %d", length);
+    int height;
+    printf("Enter rectangle height: ");
+    scanf("%d", &height);
+    printf("The length of the rectangle is %d\n", length);
+    printf("The rectangle heigth is %d\n", height);
 }
