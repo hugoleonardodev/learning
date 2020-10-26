ce719ab0722603b13858f912b1ce27bab262d78c
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-25 21:37:48 -0300

    Add arrayZeroAteCem.js

---
+++ js/arrayZeroAteCem.js
@@ -0,0 +1,10 @@
+var numeroZeroAteCem = [];
+// cria um Array (conjunto) vazio de nome numeroZaroAteCem           
+for (var index = 0; index <= 100; index ++) {
+    numero = index;
+    for (var contador = 0; contador < 1; contador++) {
+        numeroZeroAteCem.push(numero);
+    }
+}
+console.log(numeroZeroAteCem);
+// conta todos os numeros de 0 até 100 e coloca dentro do Array numerosZeroATeCem
