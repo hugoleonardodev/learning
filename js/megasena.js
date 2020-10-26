f686002fc3ef57f127d0bd10cd878d18e150055a
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-26 17:05:09 -0300

    Create an Array for the six random numbers

--- js/megasena.js
+++ js/megasena.js
@@ -8,11 +8,13 @@ console.log(randomNumber);
 console.log(Math.ceil(randomNumber));
 // if 0 was included, Math.ceil for lower integer
 // Variables receives numbers from 1 to 60
-let firstNumber;
-let secondNumber;
-let thirdNumber;
-let fourthNumber;
-let fifthNumber;
-let sixthNumber;
+let firstNumber = Math.ceil(Math.random() * 60);
+let secondNumber = Math.ceil(Math.random() * 60);
+let thirdNumber = Math.ceil(Math.random() * 60);
+let fourthNumber = Math.ceil(Math.random() * 60);
+let fifthNumber = Math.ceil(Math.random() * 60);
+let sixthNumber = Math.ceil(Math.random() * 60);
+
+console.log(fifthNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber);
 
 
