8285e9a9b5364d9a492582215a270573ec83e962
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-25 21:33:13 -0300

    Add fizz.js

---
+++ js/fizz.js
@@ -0,0 +1,11 @@
+for (var index = 0; index <= 100; index++) {
+    numeroAtual = index;
+    for (var coluna = 0; coluna < 1; coluna++) {
+if (numeroAtual % 3 == 0) {
+console.log("Fizz");
+}else{
+        console.log(numeroAtual);
+    }
+}
+}
+// conta todos os numeros de 0 até 100 e escreve "Fizz" nos numeros divisiveis por 3   
