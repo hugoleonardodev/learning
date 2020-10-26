037ca648a3d4665978f09a84cc24dd30d2b65a63
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-25 21:42:06 -0300

    Fix a bug in contaZeroCem.js

---
+++ js/contaZeroCem.js
@@ -0,0 +1,8 @@
+var numerosZeroAteCem = 0;
+for (var index = 0; index <= 100; index++){
+    numerosZeroAteCem += index;
+    for (var linha = 0; linha <= 100; linha++){
+    }
+console.log(index);
+}
+// imprime todos os números de 0 até 100 em cada linha de 0 até 100;
