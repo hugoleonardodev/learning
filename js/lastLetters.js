5404a7106f8213bddb7f2afd0b1e51f15dbae0ad
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-25 21:02:07 -0300

    corrige erros dos arquivos js e adiciona lastTwoLetters

--- js/lastLetters.js
+++ js/lastLetters.js
@@ -1,7 +1,6 @@
-function lastLetters(word) {
-    var lastLetter = word[(length)];
-    var secondLastLetter = word[(length-1)];
-    var lastLetters = lastLetter + " " + secondLastLetter  
-    return lastLetters
-    }
-    // função que retorna as duas ultimas letras de uma string em ordem invertida com espaço entre elas
+var word = "APPLE";
+var lastLetter = word[(word.length-1)];
+var secondLastLetter = word[(word.length-2)];
+var twolastLetters = lastLetter + " " + secondLastLetter;
+console.log(twolastLetters);
+// função que retorna as duas ultimas letras de uma string em ordem invertida com espaço entre elas
---
+++ js/reverseWord.js
@@ -0,0 +1,7 @@
+var word = "APLLE";
+var reverseWord = "";
+for (var index = word.length - 1; index >= 0; index--) { 
+reverseWord += word[index]
+}
+console.log(reverseWord);
+// escreve a string (word) ao contrário Ex: APPLE retorna ELPPA
