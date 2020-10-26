e72398bc293df4b81e67c33e531799eac08a56d5
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-25 21:46:20 -0300

    Add imprimeTrianguloAsterisco.js

---
+++ js/imprimeTrianguloAsterisco.js
@@ -0,0 +1,10 @@
+var linhaDeAsteriscos = "";
+var n = 15;
+for (var index = 0; index < 1; index++){
+for (var linha = 0; linha < n; linha++){
+    linhaDeAsteriscos += " *";
+    console.log(linhaDeAsteriscos);
+    }
+}
+
+// imprime um triângulo de asterisicos com parâmentro n igual a um número inteiro
